3a443245689f95d069da0c964ff182457229c2bc
nurzhanduisenbaev480 <nurzhanduisenbaev480@mail.ru>
2023-01-27 16:30:45 +0600

    Key and Mouse Codes...

---
+++ Gamerlizard/src/Gamerlizard/KeyCodes.h
@@ -0,0 +1,133 @@
+#pragma once
+
+/* From glfw3.h */
+
+
+/* The unknown key */
+#define GL_KEY_UNKNOWN            -1
+
+/* Printable keys */
+#define GL_KEY_SPACE              32
+#define GL_KEY_APOSTROPHE         39  /* ' */
+#define GL_KEY_COMMA              44  /* , */
+#define GL_KEY_MINUS              45  /* - */
+#define GL_KEY_PERIOD             46  /* . */
+#define GL_KEY_SLASH              47  /* / */
+#define GL_KEY_0                  48
+#define GL_KEY_1                  49
+#define GL_KEY_2                  50
+#define GL_KEY_3                  51
+#define GL_KEY_4                  52
+#define GL_KEY_5                  53
+#define GL_KEY_6                  54
+#define GL_KEY_7                  55
+#define GL_KEY_8                  56
+#define GL_KEY_9                  57
+#define GL_KEY_SEMICOLON          59  /* ; */
+#define GL_KEY_EQUAL              61  /* = */
+#define GL_KEY_A                  65
+#define GL_KEY_B                  66
+#define GL_KEY_C                  67
+#define GL_KEY_D                  68
+#define GL_KEY_E                  69
+#define GL_KEY_F                  70
+#define GL_KEY_G                  71
+#define GL_KEY_H                  72
+#define GL_KEY_I                  73
+#define GL_KEY_J                  74
+#define GL_KEY_K                  75
+#define GL_KEY_L                  76
+#define GL_KEY_M                  77
+#define GL_KEY_N                  78
+#define GL_KEY_O                  79
+#define GL_KEY_P                  80
+#define GL_KEY_Q                  81
+#define GL_KEY_R                  82
+#define GL_KEY_S                  83
+#define GL_KEY_T                  84
+#define GL_KEY_U                  85
+#define GL_KEY_V                  86
+#define GL_KEY_W                  87
+#define GL_KEY_X                  88
+#define GL_KEY_Y                  89
+#define GL_KEY_Z                  90
+#define GL_KEY_LEFT_BRACKET       91  /* [ */
+#define GL_KEY_BACKSLASH          92  /* \ */
+#define GL_KEY_RIGHT_BRACKET      93  /* ] */
+#define GL_KEY_GRAVE_ACCENT       96  /* ` */
+#define GL_KEY_WORLD_1            161 /* non-US #1 */
+#define GL_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define GL_KEY_ESCAPE             256
+#define GL_KEY_ENTER              257
+#define GL_KEY_TAB                258
+#define GL_KEY_BACKSPACE          259
+#define GL_KEY_INSERT             260
+#define GL_KEY_DELETE             261
+#define GL_KEY_RIGHT              262
+#define GL_KEY_LEFT               263
+#define GL_KEY_DOWN               264
+#define GL_KEY_UP                 265
+#define GL_KEY_PAGE_UP            266
+#define GL_KEY_PAGE_DOWN          267
+#define GL_KEY_HOME               268
+#define GL_KEY_END                269
+#define GL_KEY_CAPS_LOCK          280
+#define GL_KEY_SCROLL_LOCK        281
+#define GL_KEY_NUM_LOCK           282
+#define GL_KEY_PRINT_SCREEN       283
+#define GL_KEY_PAUSE              284
+#define GL_KEY_F1                 290
+#define GL_KEY_F2                 291
+#define GL_KEY_F3                 292
+#define GL_KEY_F4                 293
+#define GL_KEY_F5                 294
+#define GL_KEY_F6                 295
+#define GL_KEY_F7                 296
+#define GL_KEY_F8                 297
+#define GL_KEY_F9                 298
+#define GL_KEY_F10                299
+#define GL_KEY_F11                300
+#define GL_KEY_F12                301
+#define GL_KEY_F13                302
+#define GL_KEY_F14                303
+#define GL_KEY_F15                304
+#define GL_KEY_F16                305
+#define GL_KEY_F17                306
+#define GL_KEY_F18                307
+#define GL_KEY_F19                308
+#define GL_KEY_F20                309
+#define GL_KEY_F21                310
+#define GL_KEY_F22                311
+#define GL_KEY_F23                312
+#define GL_KEY_F24                313
+#define GL_KEY_F25                314
+#define GL_KEY_KP_0               320
+#define GL_KEY_KP_1               321
+#define GL_KEY_KP_2               322
+#define GL_KEY_KP_3               323
+#define GL_KEY_KP_4               324
+#define GL_KEY_KP_5               325
+#define GL_KEY_KP_6               326
+#define GL_KEY_KP_7               327
+#define GL_KEY_KP_8               328
+#define GL_KEY_KP_9               329
+#define GL_KEY_KP_DECIMAL         330
+#define GL_KEY_KP_DIVIDE          331
+#define GL_KEY_KP_MULTIPLY        332
+#define GL_KEY_KP_SUBTRACT        333
+#define GL_KEY_KP_ADD             334
+#define GL_KEY_KP_ENTER           335
+#define GL_KEY_KP_EQUAL           336
+#define GL_KEY_LEFT_SHIFT         340
+#define GL_KEY_LEFT_CONTROL       341
+#define GL_KEY_LEFT_ALT           342
+#define GL_KEY_LEFT_SUPER         343
+#define GL_KEY_RIGHT_SHIFT        344
+#define GL_KEY_RIGHT_CONTROL      345
+#define GL_KEY_RIGHT_ALT          346
+#define GL_KEY_RIGHT_SUPER        347
+#define GL_KEY_MENU               348
+
+#define GL_KEY_LAST               GL_KEY_MENU
---
+++ Gamerlizard/src/Gamerlizard/MouseButtonCodes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/* From GL4.h*/
+#define GL_MOUSE_BUTTON_1         0
+#define GL_MOUSE_BUTTON_2         1
+#define GL_MOUSE_BUTTON_3         2
+#define GL_MOUSE_BUTTON_4         3
+#define GL_MOUSE_BUTTON_5         4
+#define GL_MOUSE_BUTTON_6         5
+#define GL_MOUSE_BUTTON_7         6
+#define GL_MOUSE_BUTTON_8         7
+#define GL_MOUSE_BUTTON_LAST      GL_MOUSE_BUTTON_8
+#define GL_MOUSE_BUTTON_LEFT      GL_MOUSE_BUTTON_1
+#define GL_MOUSE_BUTTON_RIGHT     GL_MOUSE_BUTTON_2
+#define GL_MOUSE_BUTTON_MIDDLE    GL_MOUSE_BUTTON_3
